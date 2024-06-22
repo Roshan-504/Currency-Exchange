d2d53d671220a7d91e5510a0ec37a71c6154224f
Roshan-504 <163719813+Roshan-504@users.noreply.github.com>
2024-06-22 23:06:25 +0530

    Add files via upload

---
+++ currency.js
@@ -0,0 +1,221 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+let URL = "https://v6.exchangerate-api.com/v6/ce00200d8907207a8571143f/latest/";
+let submit = document.getElementById("submit");
+let From = document.getElementById("From");
+let To = document.getElementById("To");
+let msg = document.getElementsByClassName("msg");
+
+
+let select = document.querySelectorAll("select");
+for(let se of select){
+    for(currency in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerHTML = currency;
+        newOption.value = currency;
+        if(se.name==="From" && currency==="USD"){
+            newOption.selected = "selected"
+        }
+        if(se.name==="To" && currency==="INR"){
+            newOption.selected = "selected"
+        }
+
+        se.append(newOption)
+    }
+    se.addEventListener("change",(evt) => {
+        changeFlag(evt.target)
+    })
+}
+
+function changeFlag(element){
+    let changedCurrency = element.value
+    let fromImage = document.getElementById("fromImage");
+    let toImage = document.getElementById("toImage");
+    if (element === From){
+        fromImage.src = `https://flagsapi.com/${countryList[changedCurrency]}/flat/64.png`;
+    }
+    if (element === To){
+        toImage.src = `https://flagsapi.com/${countryList[changedCurrency]}/flat/64.png`;
+    }
+}
+
+submit.addEventListener("click" ,async (evt) => {
+    evt.preventDefault();
+    let input = document.getElementById("input");
+    if(input.value<=0){
+        input.value = 1
+    }
+    console.log(input.value)
+    let baseCurrency = URL+From.value;
+    let response = await fetch(baseCurrency)
+    let data = await response.json()
+    let rates = data.conversion_rates
+    let amount = input.value*rates[To.value]
+    msg[0].innerHTML = input.value+ " " + From.value +" = " + amount + " " + To.value
+})
+
+
+
+
+
+
+
+
